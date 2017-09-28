e9bb50cf4157dd0d2dd6cb07721b27e6e8b7462a
BenNoonan1991 <ben.noonan2@gmail.com>
2017-09-28 20:44:33 +0100

    Add and pass test for verse 1, add case statement to verse method

--- bottles_spec.rb
+++ bottles_spec.rb
@@ -26,5 +26,13 @@ class BottlesTest < Minitest::Test
       "Take one down and pass it around, " +
       "1 bottle of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_verse_1
+    expected = "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+      assert_equal expected, Bottles.new.verse(1)
   end 
 end
