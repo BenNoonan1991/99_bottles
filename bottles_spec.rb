433779b3cbad8f990c74e8a82bf7048845abcefc
BenNoonan1991 <ben.noonan2@gmail.com>
2017-09-29 14:20:41 +0100

    Remove hard coded numbers from verse 2

--- bottles_spec.rb
+++ bottles_spec.rb
@@ -76,3 +76,31 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verses(2, 0)
   end
 end 
+
+#   def test_song
+#     expected = <<-SONG
+# 99 bottles of beer on the wall, 99 bottles of beer.
+# Take one down and pass it around, 98 bottles of beer on the wall.
+# 98 bottles of beer on the wall, 98 bottles of beer.
+# Take one down and pass it around, 97 bottles of beer on the wall.
+# 97 bottles of beer on the wall, 97 bottles of beer.
+# Take one down and pass it around, 96 bottles of beer on the wall.
+#
+# # ...
+#
+# 4 bottles of beer on the wall, 4 bottles of beer.
+# Take one down and pass it around, 3 bottles of beer on the wall.
+# 3 bottles of beer on the wall, 3 bottles of beer.
+# Take one down and pass it around, 2 bottles of beer on the wall.
+#
+# 2 bottles of beer on the wall, 2 bottles of beer.
+# Take one down and pass it around, 1 bottle of beer on the wall.
+#
+# 1 bottle of beer on the wall, 1 bottle of beer.
+# Take it down and pass it around, no more bottles of beer on the wall.
+# No more bottles of beer on the wall, no more bottles of beer.
+# Go to the store and buy some more, 99 bottles of beer on the wall.
+#     SONG
+#     assert_equal expected, Bottles.new.song
+#   end
+# end
