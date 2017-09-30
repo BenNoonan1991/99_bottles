b180345051ca740f65f250db18c141e31133e252
BenNoonan1991 <ben.noonan2@gmail.com>
2017-09-30 16:22:09 +0100

    Delete unnecessary 2 case

--- bottles.rb
+++ bottles.rb
@@ -6,11 +6,6 @@ class Bottles
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
-    when 2
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number-1} #{container(number-1)} of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
