31ac30b27b7578f01ba746b9945c34e251252da8
BenNoonan1991 <ben.noonan2@gmail.com>
2017-09-30 15:45:18 +0100

    Refactor to make case 1 and else more similar

--- bottles.rb
+++ bottles.rb
@@ -7,8 +7,8 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     when 2
@@ -17,8 +17,8 @@ class Bottles
       "Take one down and pass it around, " +
       "#{number-1} #{container(number-1)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take one down and pass it around, " +
       "#{number-1} #{container(number-1)} of beer on the wall.\n"
     end
