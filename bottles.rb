11501b8aa2d766acae6137743d1f6dcf6a372133
BenNoonan1991 <ben.noonan2@gmail.com>
2017-10-03 21:11:08 +0100

    Add to_s method and refctor verse method

--- bottles.rb
+++ bottles.rb
@@ -3,10 +3,11 @@ class Bottles
   def verse(number)
     bottle_number = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
-      "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
-      "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
-      "#{bottle_number.action}, " +
-      "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+
+    "#{bottle_number} ".capitalize + "of beer on the wall, " +
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{next_bottle_number} " + "of beer on the wall.\n"
   end
 
   def verses(starting, ending)
@@ -64,6 +65,10 @@ class BottleNumber
     else
       number - 1
     end
+  end
+
+  def to_s
+    "#{quantity} #{container}"
   end
 
 end
