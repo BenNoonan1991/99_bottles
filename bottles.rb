e5e24d738f6ea1fc96b2b761c1d5792921a06ec3
BenNoonan1991 <ben.noonan2@gmail.com>
2017-10-07 19:03:12 +0100

    Use new factory

--- bottles.rb
+++ bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
+    next_bottle_number = BottleNumber.for(bottle_number.successor)
 
     "#{bottle_number} ".capitalize + "of beer on the wall, " +
     "#{bottle_number} of beer.\n" +
